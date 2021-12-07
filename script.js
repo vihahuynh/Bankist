96a34c71f40e6091e76d77422a201b2a95895c6d
vihahuynh <huynhviha1703@gmail.com>
2021-12-07 17:59:29 +0700

    sticky navigation

--- script.js
+++ script.js
@@ -116,3 +116,34 @@ const handleHover = (e, opacity) => {
 
 nav.addEventListener('mouseover', e => handleHover(e, 0.5));
 nav.addEventListener('mouseout', e => handleHover(e, 1));
+
+// Sticky navigation - Intersection Observer API
+
+// const obsCallback = (entries, observer) => {
+//   entries.forEach(entry => console.log(entry));
+// };
+
+// const obsOptions = {
+//   root: null,
+//   threshold: [0, 0.2],
+// };
+
+// const observer = new IntersectionObserver(obsCallback, obsOptions);
+// observer.observe(section1);
+
+const header = document.querySelector('header');
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = entries => {
+  const [entry] = entries;
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
