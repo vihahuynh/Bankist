e06c25721f865777bce31a14404b0f3e82892745
vihahuynh <huynhviha1703@gmail.com>
2021-12-08 16:57:41 +0700

    DOM life cycle

--- script.js
+++ script.js
@@ -163,7 +163,7 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 
 document.querySelectorAll('.section').forEach(section => {
   sectionObserver.observe(section);
-  // section.classList.add('section--hidden');
+  section.classList.add('section--hidden');
 });
 
 // Lazy loading image
@@ -265,3 +265,17 @@ const init = () => {
 };
 
 init();
+
+document.addEventListener('DOMContentLoaded', e => {
+  console.log('HTML parsed and DOM tree built!', e);
+});
+
+window.addEventListener('load', e => {
+  console.log('Page fully loaded!', e);
+});
+
+// window.addEventListener('beforeunload', e => {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = '';
+// });
