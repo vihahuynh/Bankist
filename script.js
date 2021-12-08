16b3769bf3fae9c34e744ff8f5fd4f55a8c929e2
vihahuynh <huynhviha1703@gmail.com>
2021-12-08 11:04:44 +0700

    lazy loading images

--- script.js
+++ script.js
@@ -165,3 +165,24 @@ document.querySelectorAll('.section').forEach(section => {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+
+// Lazy loading image
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = (entries, observer) => {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', () => {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0.3,
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
